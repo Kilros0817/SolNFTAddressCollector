1957ae2abcdabf0cf52876dd910557e5049f858f
Kilros0817 <lazarkitfrl@gmail.com>
2022-08-01 17:42:56 +0800

    create repository

---
+++ index.js
@@ -0,0 +1,34 @@
+const solanaWeb3 = require("@solana/web3.js");
+const fs = require('fs');
+const searchAddress = "CYpwfWrLXM7r5rgLUsbNQq6s8syGdV4WnwF55hT6i5y7"; //example 'vines1vzrYbzLMRdu58ou5XTby4qAqVRLmqo36NKPTg'
+
+const endpoint =
+  "https://hidden-small-pine.solana-mainnet.discover.quiknode.pro/979ee002d0765c17d445ac307859f16bcb5868b5/";
+const solanaConnection = new solanaWeb3.Connection(endpoint);
+
+const getTransactions = async (address) => {
+  const pubKey = new solanaWeb3.PublicKey(address);
+  let transactionList = await solanaConnection.getSignaturesForAddress(
+    pubKey,
+    {},
+    "confirmed"
+  );
+  let tokenList = [];
+  transactionList.forEach(async (transaction, i) => {
+    const tx = await solanaConnection.getParsedTransaction(
+      transaction.signature
+    );
+    const slot = tx.slot;
+    const err = tx.meta.err;
+    
+    if (slot > 143540679 && err == null) {
+      const accountKeys = tx.transaction.message.instructions[0].accounts;
+      const tokenAddress = accountKeys[5].toBase58();
+      fs.appendFile('tokenList.txt',  `${tokenAddress}\n`, function (err) {
+        if (err) throw err;
+      });
+    }
+  });
+};
+
+getTransactions(searchAddress);
